8087384340f8ab93c04d7aa4f2995d79642585d8
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2022-09-15 17:14:08 +0200

    [build] add versioneer

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
+import versioneer
 
 setup(name='scusage',
-    version='0.1',
     license='MIT',
     packages=['scusage'],
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     zip_safe=False
 )
