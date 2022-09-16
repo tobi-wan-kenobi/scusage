59d1478fee68fbd22e03e370cd6edf5be0f76618
tobi-wan-kenobi <tobi-wan-kenobi@users.noreply.github.com>
2022-09-16 11:23:59 +0200

    [setup] add dependencies, fix package build

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import versioneer
 
-setup(name='scusage',
-    license='MIT',
-    packages=['scusage'],
+setup(name="scusage",
+    license="MIT",
+    packages=find_packages(exclude=["tests"]),
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
+    install_requires=[
+        "tabulate", "i3ipc"
+    ],
     zip_safe=False
 )
